582258014041ba732af20c91ba71c14e2008a926
Edward Han <ehan1990@gmail.com>
2015-05-11 00:32:20 -0700

    added lecture 1, introduction to python

---
+++ src/lecture/Intro.py
@@ -0,0 +1,17 @@
+# everthing after the '#' is a comment. meaning lines that start with '#' will be ignored.
+# so you can write whatever you want
+# developers use this to write comments, like what this piece of code does, and etc
+
+"""
+You can use three " marks to indicate multiple comment lines
+like this, so you don't have to write '#' on every line.
+"""
+
+# pycharm automatically inserts the line below every time you create a file. you can ignore this line.
+__author__ = 'ehan'
+
+# this is the main function. when a python file is run, it firsts look for 'if __name__ == "__main__"
+# and executes the lines below it
+if __name__ == "__main__":
+    # prints "Hello World!!" in the output, also called the console.
+    print "Hello World!!"
