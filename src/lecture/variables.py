2089b4270794f9612d9dcb27098483df655403ed
Edward Han <ehan@istuary.com>
2015-05-12 13:28:40 -0700

    added variable lecture (not done)

---
+++ src/lecture/variables.py
@@ -0,0 +1,21 @@
+__author__ = 'ehan'
+
+if __name__ == "__main__":
+    #integer
+    x = 10
+    y = 8
+    z = x + y
+    z = x - y
+    z = x * y
+    z = x / y
+
+    #float
+    a = 10
+    b = 8
+    c = a + y
+    c = a - y
+    c = a * y
+    c = a / y
+
+    #string
+    edward = "hi"
