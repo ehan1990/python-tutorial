ff2cb1240203a2f5bc4d98350780ff606cfebbf8
Edward Han <ehan@istuary.com>
2015-05-13 13:30:15 -0700

    added array

---
+++ src/lecture/array.py
@@ -0,0 +1,16 @@
+__author__ = 'ehan'
+
+
+def main():
+    even_numbers = [0, 2, 4, 6, 8]
+    odd_numbers = [1, 3, 5, 7, 9]
+    cities = ["vancouver", "beijing"]
+    print even_numbers
+    print odd_numbers
+    print cities
+    print "I was born in " + cities[1] + " but now I live in " + cities[0]
+    length_of_even_numbers = len(even_numbers)
+    print "there are " + str(length_of_even_numbers) + " in the array: " + str(even_numbers)
+
+if __name__ == "__main__":
+    main()
